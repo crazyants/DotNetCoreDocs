10d0885a216c78c842c6bf9bb275d68d8457e56c
Jared Nance <jaredcnance@gmail.com>
2016-11-28 20:34:11 -0600

    fix(ci): version numbers

--- Build.ps1
+++ Build.ps1
@@ -61,4 +61,4 @@ $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 exec { & dotnet test .\test\DotNetCoreDocsTests -c Release }
 
-exec { & dotnet pack .\src\DotNetCoreDocs -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\DotNetCoreDocs -c Release -o .\artifacts --version-suffix=alpha+$revision }
