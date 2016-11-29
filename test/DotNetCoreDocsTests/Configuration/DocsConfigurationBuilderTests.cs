89532e9b66b71779098d7ebb15c3e1c5bf1b3738
Jared Nance <jaredcnance@gmail.com>
2016-11-28 20:33:58 -0600

    tests(configuration): add missing configuration tests

---
+++ test/DotNetCoreDocsTests/Configuration/DocsConfigurationBuilderTests.cs
@@ -0,0 +1,52 @@
+using System;
+using DotNetCoreDocs.Configuration;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Xunit;
+
+namespace DotNetCoreDocsTests
+{
+    public class DocsConfigurationBuilderTests
+    {
+        [Theory]
+        [InlineData("DocsConfiguration:RequestsDirectory", "")]
+        [InlineData("DocsConfiguration:RequestsDirectory", null)]
+        public void GetConfiguration_ThrowsException_IfRequiredParametersNotSpecified(string parameter, string returnValue)
+        {
+            // arrange
+            var configMock = new Mock<IConfiguration>();
+            configMock.Setup(c=> c[parameter]).Returns(returnValue);
+
+            // act
+            // assert
+            Assert.Throws<ArgumentException>(() => 
+                DocsConfigurationBuilder.GetConfiguration(configMock.Object)
+            );
+        }
+
+        [Fact]
+        public void GetConfiguration_UsesDefaultValues_IfNotProvided()
+        {
+            // arrange
+            var configMock = GetConfigMockWithRequiredParameters();
+            configMock.Setup(c=> c["DocsConfiguration:BaseAddress"]).Returns("");
+            configMock.Setup(c=> c["DocsConfiguration:DocumentationRoute"]).Returns("");
+            configMock.Setup(c=> c["DocsConfiguration:ReadmePath"]).Returns("");
+
+            // act
+            var result = DocsConfigurationBuilder.GetConfiguration(configMock.Object);
+
+            // assert
+            Assert.Equal("http://localhost:5000", result.BaseAddress);
+            Assert.Equal("/docs", result.DocumentationRoute);
+            Assert.Equal("README.md", result.ReadmePath);
+        }
+
+        private Mock<IConfiguration> GetConfigMockWithRequiredParameters()
+        {
+            var configMock = new Mock<IConfiguration>();
+            configMock.Setup(c=> c["DocsConfiguration:RequestsDirectory"]).Returns("some-value");
+            return configMock;
+        }
+    }
+}
